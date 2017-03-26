667c82d7152ffe7ee068ed873f751d993c5a27a7
Sanster <cwq1913@163.com>
2017-03-26 20:51:44 +0800

    init, make socket io work

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var path = require('path')
+
+function resolve (dir) {
+  return path.join(__dirname, '.', dir)
+}
+
+module.exports = {
+  entry: {
+    app: './src/client/index.js'
+  },
+  output: {
+    path: resolve('src/client/dist'),
+    filename: '[name].js',
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      '@': resolve('src'),
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [resolve('src/client')]
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader!sass-loader'
+      }
+    ]
+  }
+}
